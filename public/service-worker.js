73dd8d9cb0d3d974961afbc31770993a4beffbdc
magic990619 <magic990619@outlook.com>
2019-03-09 18:28:43 +1000

    gits

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.710e6bea004eedf30f912e4c2e73099c.js"
+  "/precache-manifest.f253cfd64efa39746028803cf8098045.js"
 );
 
 workbox.clientsClaim();
