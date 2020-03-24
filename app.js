e18793b1fb2a25e04c2cc1891c085a8508ae9c9f
Magic <magicrjh0619@gmail.com>
2020-03-24 06:08:45 -0700

    use request header

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const app = express();
 
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var cookieParser = require('cookie-parser');
 
 var storage = multer.diskStorage({
