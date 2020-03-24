2dcc3db5360002fe62603f3cd2b43f3f5a75006f
Magic <magicrjh0619@gmail.com>
2020-03-24 07:15:40 -0700

    change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors())
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, content-type, Accept");
     next();
 });
 
